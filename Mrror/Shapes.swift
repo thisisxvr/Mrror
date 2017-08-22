5660c745e2f3456e1600e994f30bb3c4022dcb5d
Xavier Francis <thisisxvr@gmail.com>
2017-08-22 15:13:28 +1200

    Refactor Shapes

--- Mrror/Shapes.swift
+++ Mrror/Shapes.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 Xavier Francis. All rights reserved.
 //
 
+import UIKit
+
 enum Shapes: Int {
     case freeHand = 0
     case line
@@ -13,4 +15,35 @@ enum Shapes: Int {
     case square
     case rectangle
     case triangle
+}
+
+class Shape: UIBezierPath {
+    func line(startPoint: CGPoint, endPoint: CGPoint) -> UIBezierPath {
+        let linePath = UIBezierPath()
+        linePath.move(to: CGPoint(x: startPoint.x, y: startPoint.y))
+        linePath.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y))
+        return linePath
+    }
+    
+    func oval(startPoint: CGPoint, translationPoint: CGPoint) -> UIBezierPath {
+        return UIBezierPath(ovalIn: CGRect(
+            x: startPoint.x,
+            y: startPoint.y,
+            width: translationPoint.x,
+            height: translationPoint.y))
+    }
+    
+    func rectangle(startPoint: CGPoint, translationPoint: CGPoint) -> UIBezierPath {
+        return UIBezierPath(rect: CGRect(
+            x: startPoint.x,
+            y: startPoint.y,
+            width: translationPoint.x,
+            height: translationPoint.y))
+    }
+    
+    //    func square(startPoint: CGPoint, endPoint: CGPoint) -> UIBezierPath {
+    //    }
+    
+    //    func triangle(startPoint: CGPoint, endPoint: CGPoint) -> UIBezierPath {
+    //    }
 }
